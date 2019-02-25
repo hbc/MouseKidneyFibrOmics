461152221eaf2d56198279615ced00f563030dc4
Lorena Pantano <lorena.pantano@gmail.com>
2019-02-25 15:44:47 -0500

    add databases version.

--- browser_app/ui.R
+++ browser_app/ui.R
@@ -35,9 +35,12 @@ shinyUI(fluidPage(pageWithSidebar(
 
                      HTML("Kidney fibrosis represents an urgent unmet clinical need due to the lack of effective therapies and inadequate understanding of the molecular pathogenesis. 
 We have generated a comprehensive and integrated multi-omics data set (proteomics, mRNA and small RNA transcriptomics) of fibrotic kidneys that is searchable through a user-friendly web application. Two commonly used mouse models were utilized: a reversible chemical-induced injury model (folic acid (FA) induced nephropathy) and an irreversible surgically-induced fibrosis model (unilateral ureteral obstruction (UUO)). mRNA and small RNA sequencing as well as 10-plex tandem mass tag (TMT) proteomics were performed with kidney samples from different time points over the course of fibrosis development. 
-                          The bioinformatics workflow used to process, technically validate, and integrate the single data sets is described in the manuscript and raw data files can be found on GEO GSE118341 
+                          The bioinformatics workflow used to process, technically validate, and integrate the single data sets is described in the manuscript and raw data files can be found on GEO <a href=https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE118341 >GSE118341</a>. 
                           In summary, we present temporal and integrated multi-omics data from fibrotic mouse kidneys that are accessible through an interrogation tool to provide a searchable transcriptome and proteome for kidney fibrosis researchers. 
-                          ")
+                          
+                          "),
+                     br(),
+                     p("Databases used: mirbase21 for miRNA, GRCm38.84 for mRNA, Uniprot02/04/2014 for proteins.")
                      )
         )
         
